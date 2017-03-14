699f271492c17b5b614173195b897cb12cb6accf
Joe Turner <joe@oampo.co.uk>
2017-03-14 17:24:24 +0000

    Add starting point.

--- src/components/header-bar.js
+++ src/components/header-bar.js
@@ -1,22 +1,4 @@
 import React from 'react';
 
-export default function HeaderBar(props) {
-    const links = props.links.map((link, index) => (
-        <li key={index}>
-            <a href={link.href}>
-                {link.text}
-            </a>
-        </li>
-    ));
+// The HeaderBar component goes here.  It should be the default export.
 
-    return (
-        <div className="header-bar">
-            <h1>{props.title}</h1>
-            <nav className="header-bar-nav">
-                <ul>
-                    {links}
-                </ul>
-            </nav>
-        </div>
-    );
-}
